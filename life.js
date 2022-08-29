2c3ab5c7f40af0a26788455b40f75f472db25304
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-08-29 15:42:50 +0200

    adding settings

--- life.js
+++ life.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let cw = innerWidth;
+let cw = innerWidth - 400;
 let ch = innerHeight;
 canvas.width = cw;
 canvas.height = ch;
@@ -8,24 +8,46 @@ canvas.height = ch;
 let particles = [];
 let red;
 let green;
-let yellow;
+let white;
 let blue;
 
 window.onload = function main() {
-    yellow = create(1000, 'yellow');
+    setInput()
+
+    green = create(1000, 'lime');
     red = create(1000, 'red');
-    green = create(1000, 'green');
-    blue = create(1000, 'blue');
+    white = create(1000, 'white');
+    blue = create(1000, '#4242ff');
 
     animate();
 };
 
 function rules(settings) {
-    rule(red, red, -0.1);
-    rule(red, yellow, -0.01);
-    rule(yellow, red, 0.01);
+    
 }
 
+function setInput() {
+    let numbers = Array.from(document.querySelectorAll('.number'))
+    numbers.forEach(input => {
+        input.min = "0"
+        input.max = "3000"
+        input.value = "1000"
+        input.step = "1"
+    })
+
+    let attraction = Array.from(document.querySelectorAll('.attraction'))
+    attraction.forEach(input => {
+        input.min = "-100"
+        input.max = "100"
+        input.value = "0"
+        input.step = "0.5"
+    })
+
+    let inputs = Array.from(document.querySelectorAll('input'))
+    inputs.forEach(input => {
+        input.addEventListener('change', updateRules())
+    })
+}
 
 function animate() {
     update();
@@ -36,6 +58,10 @@ function animate() {
 
 function update() {
     rules();
+}
+
+function updateRules() {
+    
 }
 
 function draw() {
@@ -62,19 +88,100 @@ function random() {
     return Math.floor(Math.random() * cw);
 }
 
-function rule(particles1, particles2, g) {
-    for (let i = 0; i < particles1.length; i++) {
+function redRule(particles, g, radius) {
+    for (let i = 0; i < red.length; i++) {
         let fx = 0;
         let fy = 0;
         let a
         let b
-        for (let j = 0; j < particles2.length; j++) {
-            a = particles1[i];
-            b = particles2[j];
+        for (let j = 0; j < particles.length; j++) {
+            a = red[i];
+            b = particles[j];
             let dx = a.x - b.x;
             let dy = a.y - b.y;
             let d = Math.sqrt(dx * dx + dy * dy);
-            if (d > 0 && d < 100) {
+            if (d > 0 && d < radius) {
+                let F = g * 1 / d;
+                fx += (F * dx);
+                fy += (F * dy);
+            }
+        }
+        a.vx = (a.vx + fx) * 0.5
+        a.vy = (a.vy + fy) * 0.5
+        a.x += a.vx;
+        a.y += a.vy;
+        if (a.x <= 0 || a.x >= cw) { a.vx *= -1 }
+        if (a.y <= 0 || a.y >= ch) { a.vy *= -1 }
+    }
+}
+
+function blueRule(particles, g, radius) {
+    for (let i = 0; i < blue.length; i++) {
+        let fx = 0;
+        let fy = 0;
+        let a
+        let b
+        for (let j = 0; j < particles.length; j++) {
+            a = blue[i];
+            b = particles[j];
+            let dx = a.x - b.x;
+            let dy = a.y - b.y;
+            let d = Math.sqrt(dx * dx + dy * dy);
+            if (d > 0 && d < radius) {
+                let F = g * 1 / d;
+                fx += (F * dx);
+                fy += (F * dy);
+            }
+        }
+        a.vx = (a.vx + fx) * 0.5
+        a.vy = (a.vy + fy) * 0.5
+        a.x += a.vx;
+        a.y += a.vy;
+        if (a.x <= 0 || a.x >= cw) { a.vx *= -1 }
+        if (a.y <= 0 || a.y >= ch) { a.vy *= -1 }
+    }
+}
+
+function blueRule(particles, g, radius) {
+    for (let i = 0; i < blue.length; i++) {
+        let fx = 0;
+        let fy = 0;
+        let a
+        let b
+        for (let j = 0; j < particles.length; j++) {
+            a = blue[i];
+            b = particles[j];
+            let dx = a.x - b.x;
+            let dy = a.y - b.y;
+            let d = Math.sqrt(dx * dx + dy * dy);
+            if (d > 0 && d < radius) {
+                let F = g * 1 / d;
+                fx += (F * dx);
+                fy += (F * dy);
+            }
+        }
+        a.vx = (a.vx + fx) * 0.5
+        a.vy = (a.vy + fy) * 0.5
+        a.x += a.vx;
+        a.y += a.vy;
+        if (a.x <= 0 || a.x >= cw) { a.vx *= -1 }
+        if (a.y <= 0 || a.y >= ch) { a.vy *= -1 }
+    }
+}
+
+function greenRule(particles, g, radius) {
+    for (let i = 0; i < green.length; i++) {
+        let fx = 0;
+        let fy = 0;
+        let a
+        let b
+        for (let j = 0; j < particles.length; j++) {
+            a = green[i];
+            b = particles[j];
+            let dx = a.x - b.x;
+            let dy = a.y - b.y;
+            let d = Math.sqrt(dx * dx + dy * dy);
+            if (d > 0 && d < radius) {
                 let F = g * 1 / d;
                 fx += (F * dx);
                 fy += (F * dy);
